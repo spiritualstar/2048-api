8f9d2380dc83f08a3fcce19c9b7966ea9428c45b
spiritualstar <44721985+spiritualstar@users.noreply.github.com>
2019-01-12 00:15:20 +0800

    Add files via upload

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,6 +1,14 @@
 import numpy as np
+import keras
+from keras.models import load_model
 
-
+model1=load_model('2048_new2_2048.h5')
+#my_model32=load_model('2048_32_1.h5')
+#my_model64=load_model('2048_64_1.h5')
+#my_model128=load_model('2048_128.h5')
+#my_model256=load_model('2048_256.h5')
+#my_model512=load_model('2048_512.h5')
+#my_model1024=load_model('2048_1024.h5')
 class Agent:
     '''Agent Base.'''
 
@@ -20,7 +28,7 @@ class Agent:
                     ["left", "down", "right", "up"][direction]))
                 if self.display is not None:
                     self.display.display(self.game)
-
+ 
     def step(self):
         direction = int(input("0: left, 1: down, 2: right, 3: up = ")) % 4
         return direction
@@ -46,3 +54,21 @@ class ExpectiMaxAgent(Agent):
     def step(self):
         direction = self.search_func(self.game.board)
         return direction
+
+class MyOwnAgent(Agent):
+    def __init__(self, game, display=None):
+        self.game = game
+        self.display = display
+    def step(self):
+       
+        x_train=np.array(self.game.board)
+        x=x_train
+        x=np.log2(x+1)
+        x=np.trunc(x)
+        x = keras.utils.to_categorical(x,12)
+        x = x.reshape(1, 4, 4, 12)
+        pred=model1.predict(x,batch_size=128)
+        r=pred[0]
+        r1=r.tolist()
+        direction2=r1.index(max(r1))
+        return direction2            
